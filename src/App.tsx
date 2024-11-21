f5352219a243b2014ac5b804400541aaaa150326
Jacob Boot <Jacob.Boot@dunelm.com>
2024-11-21 12:10:25 +0000

    Fix lint error

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,9 @@ function App() {
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <button onClick={createTodo} type="button">
+        + new
+      </button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
