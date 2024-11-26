d65046d9216d88d35a522776dd2134f066268ad3
Jacob Boot <Jacob.Boot@dunelm.com>
2024-11-26 16:44:40 +0000

    Add Auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react'
 import type { Schema } from '../amplify/data/resource'
 import { Button, DarkThemeToggle, Flowbite, ListGroup } from 'flowbite-react'
 import { generateClient } from 'aws-amplify/api'
+import { Authenticator } from '@aws-amplify/ui-react'
+import { signOut } from 'aws-amplify/auth'
+import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>()
 
@@ -18,28 +21,35 @@ const App = () => {
     client.models.Todo.create({ content: window.prompt('Todo content') })
   }
 
+  const handleClick = () => {
+    signOut()
+  }
+
   return (
-    <main className="flex gap-2 flex-col p-10">
-      <Flowbite>
-        <h1>My todos</h1>
-        <Button pill onClick={createTodo}>
-          + new
-        </Button>
-        <ListGroup>
-          {todos.map((todo) => (
-            <ListGroup.Item key={todo.id}>{todo.content}</ListGroup.Item>
-          ))}
-        </ListGroup>
-        <div>
-          🥳 App successfully hosted. Try creating a new todo.
-          <br />
-          <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-            Review next step of this tutorial.
-          </a>
-        </div>
-        <DarkThemeToggle />
-      </Flowbite>
-    </main>
+    <Authenticator>
+      <main className="flex gap-2 flex-col p-10">
+        <Flowbite>
+          <h1>My todos</h1>
+          <Button pill onClick={createTodo}>
+            + new
+          </Button>
+          <ListGroup>
+            {todos.map((todo) => (
+              <ListGroup.Item key={todo.id}>{todo.content}</ListGroup.Item>
+            ))}
+          </ListGroup>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+              Review next step of this tutorial.
+            </a>
+          </div>
+          <DarkThemeToggle />
+        </Flowbite>
+        <Button onClick={handleClick}>Sign Out</Button>
+      </main>
+    </Authenticator>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,20 @@ import App from './App.tsx'
 import { Amplify } from 'aws-amplify'
 import './index.css'
 import outputs from '../amplify_outputs.json'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import HomePage from './app/pages/Home/HomePage.tsx'
+import { Authenticator } from '@aws-amplify/ui-react'
 
 Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/app" element={<App />} />
+        <Route path="/login" element={<Authenticator />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
