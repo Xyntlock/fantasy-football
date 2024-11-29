d8881674b4c7d012452951368305f502df5cfc6e
Xyntlock <jacobjohn1005@gmail.com>
2024-11-29 17:38:31 +0000

    Remove unnecessary biome commands

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import '@aws-amplify/ui-react/styles.css'
+
 import { generateClient } from 'aws-amplify/api'
 import { signOut } from 'aws-amplify/auth'
 import { Button, DarkThemeToggle, Flowbite, ListGroup } from 'flowbite-react'
 import { useEffect, useState } from 'react'
 import type { Schema } from '../amplify/data/resource'
-import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>()
 
