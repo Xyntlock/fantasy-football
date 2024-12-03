6d21fbc0cc39d7be289432bf709ea9dc1afd90ef
Xyntlock <jacobjohn1005@gmail.com>
2024-12-03 16:00:55 +0000

    Move lambda call to homepage

--- src/app/pages/Home/HomePage.tsx
+++ src/app/pages/Home/HomePage.tsx
@@ -1,6 +1,14 @@
+import { generateClient } from 'aws-amplify/api'
+import type { Schema } from '../../../../amplify/data/resource'
 import Page from '../Page'
 
+const client = generateClient<Schema>()
+
 const HomePage = () => {
+  client.queries.getApi({
+    name: 'Test',
+  })
+
   return <Page>Home Page</Page>
 }
 
