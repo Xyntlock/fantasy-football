b9aaef197bab61d7aebc452693092d9779882b9d
Xyntlock <jacobjohn1005@gmail.com>
2024-12-04 16:38:06 +0000

    Query football api

--- amplify/functions/get-api/handler.ts
+++ amplify/functions/get-api/handler.ts
@@ -1,11 +1,23 @@
+import { secret } from '@aws-amplify/backend'
 import type { Handler } from 'aws-lambda'
+import axios, { type AxiosHeaderValue, type AxiosRequestConfig } from 'axios'
 import type { Schema } from '../../data/resource'
+
+const config: AxiosRequestConfig = {
+  method: 'get',
+  url: 'https://v3.football.api-sports.io/football/leagues/',
+  headers: {
+    'x-rapidapi-key': secret('football-api-key') as unknown as AxiosHeaderValue,
+    'x-rapidapi-host': 'v3.football.api-sports.io',
+  },
+}
 
 export const handler: Schema['getApi']['functionHandler'] = async (
   event,
   context
 ) => {
+  const response = await axios(config)
   // your function code goes here
   console.log(event)
-  return 'Hello, World!'
+  return response.data
 }
