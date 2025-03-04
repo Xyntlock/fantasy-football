3b8afea100a33bb2af5b1def47d8156e66fe296f
Xyntlock <jacobjohn1005@gmail.com>
2025-03-04 14:30:12 +0000

    Grab players in scheduled lambda

--- amplify/jobs/update-players/handler.ts
+++ amplify/jobs/update-players/handler.ts
@@ -1,10 +1,48 @@
+// biome-ignore lint/style/useNodejsImportProtocol: node:fetch doesn't work in Lambda
+import https from 'https'
 import type { EventBridgeHandler } from 'aws-lambda'
+import { env } from '$amplify/env/get-api'
 
 export const handler: EventBridgeHandler<
   'Scheduled Event',
   null,
   boolean
 > = async (event) => {
-  console.log('event', JSON.stringify(event, null, 2))
+  const options: https.RequestOptions = {
+    hostname: 'v3.football.api-sports.io',
+    path: '/players/squads?team=46',
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': env.API_KEY,
+      'x-rapidapi-host': 'v3.football.api-sports.io',
+    },
+  }
+
+  let players: unknown
+  const req = https.get(options, (res) => {
+    let data = ''
+    // A chunk of data has been received.
+    res.on('data', (chunk) => {
+      data += chunk
+    })
+
+    // The whole response has been received.
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(data)
+        players = parsedData
+        console.log('players', players)
+      } catch (error) {
+        throw new Error(`Error parsing JSON: ${error}`)
+      }
+    })
+  })
+
+  req.on('error', (error) => {
+    throw new Error(`Error making request: ${error.message}`)
+  })
+
+  req.end()
+
   return true
 }
--- amplify/jobs/update-players/resource.ts
+++ amplify/jobs/update-players/resource.ts
@@ -1,7 +1,10 @@
-import { defineFunction } from '@aws-amplify/backend'
+import { defineFunction, secret } from '@aws-amplify/backend'
 
 export const updatePlayers = defineFunction({
   name: 'updatePlayers',
   schedule: 'every week',
   entry: './handler.ts',
+  environment: {
+    API_KEY: secret('football-api-key'),
+  },
 })
