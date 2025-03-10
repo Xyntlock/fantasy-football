6ec05042d27930273da0eb1b3e5d93c0c7263cf3
Xyntlock <jacobjohn1005@gmail.com>
2025-03-10 16:42:55 +0000

    Change env directory name

--- amplify/jobs/update-players/resource.ts
+++ amplify/jobs/update-players/resource.ts
@@ -1,7 +1,7 @@
 import { defineFunction, secret } from '@aws-amplify/backend'
 
 export const updatePlayers = defineFunction({
-  name: 'updatePlayers',
+  name: 'update-players',
   schedule: 'every 5m',
   entry: './handler.ts',
   environment: {
