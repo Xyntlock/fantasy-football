93c21d6ec3df816b51af75cafd655303f905f0c4
Xyntlock <jacobjohn1005@gmail.com>
2025-03-24 17:03:13 +0000

    Get all players from PL

--- amplify/jobs/update-players/handler.ts
+++ amplify/jobs/update-players/handler.ts
@@ -1,11 +1,11 @@
-// biome-ignore lint/style/useNodejsImportProtocol: node:fetch doesn't work in Lambda
-import https from 'https'
 import type { EventBridgeHandler } from 'aws-lambda'
 import { env } from '$amplify/env/update-players'
 import { generateClient } from 'aws-amplify/api'
 import type { Schema } from '../../data/resource'
 import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime'
 import { Amplify } from 'aws-amplify'
+import { getOptions, request } from './request'
+import type { GetPlayersResponse } from './types'
 
 const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env)
 
@@ -13,97 +13,34 @@ Amplify.configure(resourceConfig, libraryOptions)
 
 const client = generateClient<Schema>()
 
-interface TeamSquadResponse {
-  get: string
-  parameters: {
-    team: string
-  }
-  errors: unknown[]
-  results: number
-  paging: {
-    current: number
-    total: number
-  }
-  response: TeamSquad[]
-}
-
-interface TeamSquad {
-  team: Team
-  players: Player[]
-}
-
-interface Team {
-  id: number
-  name: string
-  logo: string
-}
-
-interface Player {
-  id: number
-  name: string
-  age: number
-  number: number | null
-  position: 'Goalkeeper' | 'Defender' | 'Midfielder' | 'Attacker'
-  photo: string
-}
-
 export const handler: EventBridgeHandler<
   'Scheduled Event',
   null,
   unknown
 > = async (event) => {
-  const options: https.RequestOptions = {
-    hostname: 'v3.football.api-sports.io',
-    path: '/players/squads?team=46',
-    method: 'GET',
-    headers: {
-      'x-rapidapi-key': env.API_KEY,
-      'x-rapidapi-host': 'v3.football.api-sports.io',
-    },
-  }
+  let currentPage = 1
+  let maxPage: number
 
-  console.log('entered lambda', options)
+  const players = []
 
-  const data = await new Promise<TeamSquadResponse>((resolve, reject) => {
-    const req = https.get(options, (res) => {
-      console.log('beginning data')
-
-      let data = ''
-      // A chunk of data has been received.
-      res.on('data', (chunk) => {
-        data += chunk
-      })
-
-      // The whole response has been received.
-      res.on('end', () => {
-        try {
-          const parsedData: TeamSquadResponse = JSON.parse(data)
-          console.log('players', parsedData)
-
-          resolve(parsedData)
-        } catch (error) {
-          reject(new Error(`Error parsing JSON: ${error}`))
-        }
-      })
-    })
-
-    req.on('error', (error) => {
-      reject(new Error(`Error making request: ${error.message}`))
-    })
-
-    req.end()
-  })
+  do {
+    const options = getOptions(currentPage)
+    const playersResponse = (await request({ options })) as GetPlayersResponse
+    players.push(...playersResponse.response)
+    maxPage = playersResponse.paging.total
+    currentPage++
+  } while (currentPage <= maxPage)
 
   // TODO figure out how filtering works to filter only for `player` prefix
   const existingPlayers = (await client.models.Squads.list()).data
 
   await Promise.all(
-    data.response[0].players.map(async (player) => {
+    players.map(async ({ player, statistics }) => {
       const pk = `player#${player.id}`
 
       const mappedPlayer = {
         pk,
-        position: player.position,
+        position: statistics[0].games.position,
         name: player.name,
         photo: player.photo,
         age: player.age,
---
+++ amplify/jobs/update-players/request.ts
@@ -0,0 +1,45 @@
+// biome-ignore lint/style/useNodejsImportProtocol: node:fetch doesn't work in Lambda
+import https from 'https'
+import { env } from '$amplify/env/update-players'
+
+type RequestArgs = {
+  options: https.RequestOptions
+}
+
+export const getOptions = (page = 1) => ({
+  hostname: 'v3.football.api-sports.io',
+  path: `/teams?league=39&season=2024&page=${page}`,
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': env.API_KEY,
+    'x-rapidapi-host': 'v3.football.api-sports.io',
+  },
+})
+
+export const request = ({ options }: RequestArgs) =>
+  new Promise((resolve, reject) => {
+    const req = https.get(options, (res) => {
+      let data = ''
+      // A chunk of data has been received.
+      res.on('data', (chunk) => {
+        data += chunk
+      })
+
+      // The whole response has been received.
+      res.on('end', () => {
+        try {
+          const parsedData = JSON.parse(data)
+
+          resolve(parsedData)
+        } catch (error) {
+          reject(new Error(`Error parsing JSON: ${error}`))
+        }
+      })
+    })
+
+    req.on('error', (error) => {
+      reject(new Error(`Error making request: ${error.message}`))
+    })
+
+    req.end()
+  })
