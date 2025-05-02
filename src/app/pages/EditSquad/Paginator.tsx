a0aca33936029f0343fae96178faf0844ce661e8
Xyntlock <jacobjohn1005@gmail.com>
2025-05-02 11:32:43 +0100

    Update Paginator

--- src/app/pages/EditSquad/Paginator.tsx
+++ src/app/pages/EditSquad/Paginator.tsx
@@ -1,25 +1,48 @@
-import { useState } from 'react'
-import type { Player, PositionEnum } from '../../../../amplify/data/resource'
+import { useEffect, useState } from 'react'
+import type {
+  Player,
+  PositionEnum,
+  Schema,
+} from '../../../../amplify/data/resource'
 import { Pagination } from 'flowbite-react'
+import type { V6Client } from '@aws-amplify/api-graphql'
 
 type PaginatorProps = {
   position: PositionEnum | null
-  client: any
+  client: V6Client<Schema>
+}
+
+type GetPageList = (params: {
+  pageNumber: number
+  list: Array<Player>
+}) => Array<Player>
+
+const getPageList: GetPageList = ({ pageNumber, list }) => {
+  const startIndex = (pageNumber - 1) * 10
+  const endIndex = startIndex + 10
+  return list.slice(startIndex, endIndex)
 }
 
 export const Paginator = ({ position, client }: PaginatorProps) => {
-  if (!position) {
-    return null
-  }
-
   const [nextToken, setNextToken] = useState<string | null | undefined>(null)
   const [currentPage, setCurrentPage] = useState(1)
   const [players] = useState<Player[]>([])
-  const [maxPage, setMaxPage] = useState(1)
+  const [maxPage, setMaxPage] = useState(0)
+
+  useEffect(() => {
+    if (position) {
+      onPageChange(1)
+    }
+  }, [position])
 
   const onPageChange = async (pageNumber: number) => {
+    if (!position) {
+      return
+    }
+
     if (pageNumber <= maxPage) {
-      console.log(players)
+      const shortlist = getPageList({ pageNumber, list: players })
+      console.log(shortlist)
     } else {
       const res = await client.models.Squads.listSquadsByPosition(
         { position },
@@ -34,11 +57,15 @@ export const Paginator = ({ position, client }: PaginatorProps) => {
     setCurrentPage(pageNumber)
   }
 
+  if (!position) {
+    return null
+  }
+
   return (
     <Pagination
       currentPage={currentPage}
       onPageChange={onPageChange}
-      layout="table"
+      layout="navigation"
       totalPages={100}
     />
   )
